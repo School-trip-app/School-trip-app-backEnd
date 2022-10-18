58ccb06f844c5bdc5d8ced46a1f49100bd327c3e
WllaTalafha <wlla.issa396@gmail.com>
2022-10-18 23:20:29 +0300

    memory modified

--- routes/memory.route.js
+++ routes/memory.route.js
@@ -8,11 +8,13 @@ router.post('/memory', addMemory);
 router.get('/memory', getMemorys);
 router.put('/memory/:id', updateMemory);
 router.delete('/memory/:id', deleteMemory);
+router.patch('/like/:id', updateLike);
+router.patch('/dislike/:id', updateDislike);
 
-function addMemory(req, res, next) {
-  // body:{"userId":"integer","imageUrl":"string","discription":"string","likes":"integer","dislikes":"integer"}
+async function addMemory(req, res, next) {
+  //body:{"userId":"integer","imageUrl":"string","discription":"string","likes":"integer","dislikes":"integer"}
   try {
-    memoriesModel.create(req.body)
+    await memoriesModel.create(req.body)
       .then(resolve => { res.status(201).send('done') })
       .catch(reject => { res.status(306).send(reject) });
   } catch (err) {
@@ -20,9 +22,9 @@ function addMemory(req, res, next) {
   }
 }
 
-function getMemorys(req, res, next) {
+async function getMemorys(req, res, next) {
   try {
-    memoriesModel.findAll({ include: [commentModel, UserModel] })
+    await memoriesModel.findAll({ include: [commentModel, UserModel] })
       .then((resolve) => {
         res.status(200).send(resolve);
       })
@@ -32,9 +34,9 @@ function getMemorys(req, res, next) {
   }
 }
 
-function updateMemory(req, res, next) {
+async function updateMemory(req, res, next) {
   try {
-    memoriesModel.update(req.body, { where: { id: req.params.id } })
+    await memoriesModel.update(req.body, { where: { id: req.params.id } })
       .then(resolve => { res.status(200).send('updated') })
       .catch(reject => { console.log(`cannot update`) });
   } catch (err) {
@@ -42,13 +44,36 @@ function updateMemory(req, res, next) {
   }
 }
 
-function deleteMemory(req, res, next) {
+async function deleteMemory(req, res, next) {
   try {
-    memoriesModel.destroy({ where: { id: req.params.id } })
+    await memoriesModel.destroy({ where: { id: req.params.id } })
       .then((resolve) => { res.status(202).send(`deleted`) })
       .catch((reject) => { console.log('Cant Delete') });
   } catch (err) {
     next(`Error inside deleteMemory function : ${err}`);
+  }
+}
+
+async function updateLike(req, res, next) {
+  try {
+    const memory = await memoriesModel.findOne({ where: { id: req.params.id } })
+    const likes = memory.likes + 1 ;
+    await memoriesModel.update({likes: likes}, { where: { id: req.params.id } })
+      .then(resolve => { res.status(200).send('updated') })
+      .catch(reject => { console.log(`cannot update`) });
+  } catch (err) {
+    next(`Error inside updateLike function : ${err}`);
+  }
+}
+async function updateDislike(req, res, next) {
+  try {
+    const memory = await memoriesModel.findOne({ where: { id: req.params.id } })
+    const dislikes = memory.dislikes + 1 ;
+    await memoriesModel.update({dislikes: dislikes}, { where: { id: req.params.id } })
+      .then(resolve => { res.status(200).send('updated') })
+      .catch(reject => { console.log(`cannot update`) });
+  } catch (err) {
+    next(`Error inside updateDislike function : ${err}`);
   }
 }
 
